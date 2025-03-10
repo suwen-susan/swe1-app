b3610db1d6fc8b410ca985309ad70d9fa2b154a3
suwen-susan <sw6359@nyu.edu>
2025-03-09 22:25:20 -0400

    Modify code to satisfy black format

--- test.py
+++ test.py
@@ -33,6 +33,7 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
+
 class QuestionDetailViewTests(TestCase):
     def test_past_question(self):
         """

72a979533e91e8497b2133e9df9f33244c49b4a5
suwen-susan <sw6359@nyu.edu>
2025-03-09 22:00:59 -0400

    Add github actions

--- test.py
+++ test.py
@@ -4,15 +4,8 @@ from django.utils import timezone
 from django.urls import reverse
 from polls.models import Question
 
-class QuestionModelTests(TestCase):
-    def test_was_published_recently_with_future_question(self):
-        """
-        was_published_recently() should return False for future-time Question
-        """
-        future_time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(pub_date=future_time)
-        self.assertIs(future_question.was_published_recently(), False)
 
+class QuestionModelTests(TestCase):
     def test_was_published_recently_with_old_question(self):
         """
         was_published_recently() should return False for Questions that are more than a day old.
@@ -25,7 +18,9 @@ class QuestionModelTests(TestCase):
         """
         was_published_recently() should return True for Questions published within one day.
         """
-        recent_time = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
+        recent_time = timezone.now() - datetime.timedelta(
+            hours=23, minutes=59, seconds=59
+        )
         recent_question = Question(pub_date=recent_time)
         self.assertIs(recent_question.was_published_recently(), True)
 
@@ -38,63 +33,7 @@ def create_question(question_text, days):
     time = timezone.now() + datetime.timedelta(days=days)
     return Question.objects.create(question_text=question_text, pub_date=time)
 
-
-class QuestionIndexViewTests(TestCase):
-    def test_no_questions(self):
-        """
-        If there are no problems, the index page should display the appropriate message.
-        """
-        response = self.client.get(reverse("polls:index"))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context["latest_question_list"], [])
-
-    def test_past_question(self):
-        """
-        Issues posted in the past should be displayed on the index page.
-        """
-        question = create_question("Past question.", days=-30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(response.context["latest_question_list"], [question])
-
-    def test_future_question(self):
-        """
-        Issues for future releases should not be displayed on the index page.
-        """
-        create_question("Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertContains(response, "No polls are available.")
-        self.assertQuerysetEqual(response.context["latest_question_list"], [])
-
-    def test_future_question_and_past_question(self):
-        """
-        If there are both past and future issues, only the past issues should be displayed on the index page.
-        """
-        past_question = create_question("Past question.", days=-30)
-        create_question("Future question.", days=30)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(response.context["latest_question_list"], [past_question])
-
-    def test_two_past_questions(self):
-        """
-        The Index page should be able to display multiple past issues.
-        """
-        question1 = create_question("Past question 1.", days=-30)
-        question2 = create_question("Past question 2.", days=-5)
-        response = self.client.get(reverse("polls:index"))
-        self.assertQuerysetEqual(response.context["latest_question_list"], [question2, question1])
-
-
 class QuestionDetailViewTests(TestCase):
-    def test_future_question(self):
-        """
-        Visiting the details page of an issue posted by future should return a 404 error.
-        """
-        future_question = create_question("Future question.", days=5)
-        url = reverse("polls:detail", args=(future_question.id,))
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
-
     def test_past_question(self):
         """
         The details page for past issues posted should display the issue content normally.
